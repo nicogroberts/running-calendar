cdc3b1d610616d0d0f4425de6b68e27ede7dadfc
nico <nicogroberts02@gmail.com>
2026-08-03 17:17:28 -0400

    added activity.js

---
+++ src/activity.js
@@ -0,0 +1,13 @@
+/**
+ * A class defining a activity object
+ */
+class Activity {
+    constructor(date, mileage, time, pace) {
+        this.date = date;
+        this.mileage = mileage;
+        this.time = time;
+        this.pace = pace;
+    }
+}
+
+export { Activity };
