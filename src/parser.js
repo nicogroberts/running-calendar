3304d1a87586d0aed680ef1ece9c06ca50c33d90
nico <nicogroberts02@gmail.com>
2026-08-03 16:09:57 -0400

    added function to get results

--- src/parser.js
+++ src/parser.js
@@ -3,12 +3,36 @@
  */
 class Parser {
     constructor() {
-
+        this.results = [];
     }
 
     parseFile(text) {
-        
+        const lines = text.split(/\r?\n/);
+
+        lines.forEach(line => {
+            const trimmedLine = line.trim();
+
+            if (trimmedLine.startsWith("- Date:")) {
+                // Get date
+            }
+
+            if (trimmedLine.startsWith("- Mileage:")) {
+                // Get mileage
+            }
+
+            if (trimmedLine.startsWith("- Time:")) {
+                // Get time check for HH:MM:SS format or MM:SS format
+            }
+
+            if (trimmedLine.startsWith("- Pace:")) {
+                // Get pace
+            }
+        });
     };
+
+    getResults() {
+        return this.results;
+    }
 }
 
 export { Parser };
