2e06753928f30788f14ba2f34ef00b0ead74305d
nico <nicogroberts02@gmail.com>
2026-08-04 16:23:48 -0400

    added logmessage.js

---
+++ src/logmessage.js
@@ -0,0 +1,11 @@
+/**
+ * A class defining a log message object
+ */
+class Logmessage {
+    constructor() {
+        this.element = document.createElement("div");
+        this.element.classList.add("log-message");
+    }
+}
+
+export { Logmessage };
