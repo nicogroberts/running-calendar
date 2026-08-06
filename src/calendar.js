6cbd73bea502abfc5883b3a9abe6b0e817d2db88
nico <nicogroberts02@gmail.com>
2026-08-06 15:12:48 -0400

    added function to convert time to seconds

--- src/calendar.js
+++ src/calendar.js
@@ -167,10 +167,22 @@ class Calendar {
     };
 
     populateCalendar(activities) {
-        
+        // need to work on this next
     };
 
-    getColor(){};
+    getColor() {};
+
+    toSeconds(time) {
+        const parts = time.split(":").map(Number);
+
+        if (parts.length === 3) {
+            return parts[0] * 3600 + parts[1] * 60 + parts[2];
+        } else if (parts.length === 2) {
+            return parts[0] * 60 + parts[1];
+        } else {
+            return 0;
+        }
+    };
 }
 
 export { Calendar };
